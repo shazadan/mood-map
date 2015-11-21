fe22e83bcb6ee3711a175524ccefb889af2c0423
shazadan <shazadanawaz@gmail.com>
2015-11-21 09:29:23 -0700

    Added django template files

---
+++ config/settings/base.py
@@ -0,0 +1,88 @@
+import os
+
+from django.core.exceptions import ImproperlyConfigured
+
+from unipath import Path
+
+
+def get_env_variable(var_name):
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(var_name)
+        raise ImproperlyConfigured(error_msg)
+
+
+BASE_DIR = Path(__file__).ancestor(3)
+
+SECRET_KEY = get_env_variable("SECRET_KEY")
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'contact_form',
+    'notes',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+)
+
+ROOT_URLCONF = 'config.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR.child("templates")],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'America/Phoenix'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATICFILES_DIRS = (
+    BASE_DIR.child("static"),
+)
+
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = BASE_DIR.child("media")
---
+++ fabfile.py
@@ -0,0 +1,115 @@
+from fabric.api import env, local, run, sudo, require
+
+APT_INSTALL_PACKAGES = [
+                        "libpq-dev",
+                        "python-dev",
+                        "python-pip",
+                        "postgresql",
+                        "postgresql-contrib",
+                        ]
+
+base = '/server'
+virtualenvs = '.virtualenvs'
+projects = 'projects'
+repo = 'django-template'
+
+# Environments
+def vagrant():
+    # change from the default user to 'vagrant'
+    env.user = 'vagrant'
+    # connect to the port-forwarded ssh
+    env.hosts = ['127.0.0.1:2222']
+    # use vagrant ssh key
+    result = local('vagrant ssh-config | grep IdentityFile', capture=True)
+    env.key_filename = result.split()[1]
+    env.base_dir = base
+    env.virtualenvs_dir = virtualenvs
+    env.virtualenv = '01'
+    env.projects_dir = projects
+    env.repo_dir = repo
+    env.settings = 'local'
+
+# Bootstrap commands
+def bootstrap():
+    require('hosts',  provided_by=[vagrant])
+    bs_install_packages()
+    bs_make_folder_structure()
+    bs_setup_virtualenv()
+    bs_link_project()
+    bs_install_requirements()
+    bs_install_heroku_toolbelt()
+
+def bs_install_packages():
+    sudo("apt-get update")
+    sudo("apt-get upgrade -y")
+    sudo("apt-get -y install " + " ".join(APT_INSTALL_PACKAGES))
+
+def bs_make_folder_structure():
+    sudo("if [ ! -d %(base_dir)s ]; then mkdir -p %(base_dir)s; chmod 777 "
+          "%(base_dir)s; fi" % env)
+    sudo("if [ ! -d %(base_dir)s/%(virtualenvs_dir)s ]; then mkdir -p %("
+          "base_dir)s/%("
+         "virtualenvs_dir)s; chmod "
+         "777 %(base_dir)s/%(virtualenvs_dir)s; fi" % env)
+    sudo("if [ ! -d %(base_dir)s/%(projects_dir)s ]; then mkdir -p %("
+          "base_dir)s/%("
+         "projects_dir)s; chmod "
+         "777 %(base_dir)s/%(projects_dir)s; fi" % env)
+
+def bs_setup_virtualenv():
+    "Fetches the virtualenv package"
+    run("if [ ! -e virtualenv-13.1.0.tar.gz ]; then wget "
+        " https://pypi.python.org/packages/source/v/virtualenv/virtualenv-13"
+        ".1.0.tar.gz; fi")
+    run("if [ ! -d virtualenv-13.1.0 ]; then tar xzf "
+         "virtualenv-13.1.0.tar.gz; fi")
+    run("rm -f virtualenv")
+    run("ln -s virtualenv-13.1.0 virtualenv")
+    run("if [ ! -d %(base_dir)s/%(virtualenvs_dir)s/%(virtualenv)s ]; then "
+         "python "
+        "~/virtualenv/virtualenv.py --no-site-packages %(base_dir)s/%("
+        "virtualenvs_dir)s/%(virtualenv)s; fi" % env)
+    sudo("chmod 777 %(base_dir)s/%(virtualenvs_dir)s/%(virtualenv)s" % env)
+
+def bs_link_project(): # vagrant only
+    run("ln -s /vagrant %(base_dir)s/%(projects_dir)s/%(repo_dir)s" % env)
+
+def bs_install_requirements():
+    sudo("source %(base_dir)s/%(virtualenvs_dir)s/%(virtualenv)s/bin/activate;"
+         "pip install -r %(base_dir)s/%(projects_dir)s/%("
+         "repo_dir)s/requirements/%(settings)s.txt;" % env)
+
+def bs_install_heroku_toolbelt():
+    run("wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh")
+
+# Django commands
+def runserver():
+    run("source %(base_dir)s/%(virtualenvs_dir)s/%("
+        "virtualenv)s/bin/activate; python %("
+        "base_dir)s/%(projects_dir)s/%(repo_dir)s/manage.py "
+        "runserver "
+        "0.0.0.0:8000 --settings=config.settings.%(settings)s" % env)
+
+def migrate():
+    run("source %(base_dir)s/%(virtualenvs_dir)s/%("
+        "virtualenv)s/bin/activate; python %("
+        "base_dir)s/%(projects_dir)s/%(repo_dir)s/manage.py "
+        "migrate --settings=config.settings.%(settings)s" % env)
+
+def collectstatic():
+    run("source %(base_dir)s/%(virtualenvs_dir)s/%("
+        "virtualenv)s/bin/activate; python %("
+        "base_dir)s/%(projects_dir)s/%(repo_dir)s/manage.py "
+        "collectstatic --settings=config.settings.%(settings)s" % env)
+
+def test():
+    run("source %(base_dir)s/%(virtualenvs_dir)s/%("
+        "virtualenv)s/bin/activate; coverage run %("
+        "base_dir)s/%(projects_dir)s/%(repo_dir)s/manage.py "
+        "test "
+        "--settings=config.settings.%(settings)s" % env)
+
+def test_report():
+    run("source %(base_dir)s/%(virtualenvs_dir)s/%(virtualenv)s/bin/activate;"
+        "cd %(base_dir)s/%(projects_dir)s//%(repo_dir)s/;"
+        "coverage html --omit='admin.py'" % env)
