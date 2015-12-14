57a0d3e23c0005daa9f63ad6eed3f7668e599390
shazadan <shazadanawaz@gmail.com>
2015-12-13 17:57:59 -0700

    changed max connection settings in production.py in BROKER_TRANSPORT_OPTIONS

--- config/settings/production.py
+++ config/settings/production.py
@@ -27,7 +27,7 @@ STATIC_ROOT = 'staticfiles'
 BROKER_URL = get_env_variable('REDIS_URL')
 CELERY_RESULT_BACKEND = get_env_variable('REDIS_URL')
 BROKER_TRANSPORT_OPTIONS = {
-    'max_connections': 20,
+    'max_connections': 15,
 }
 CELERY_REDIS_MAX_CONNECTIONS = 15
 
